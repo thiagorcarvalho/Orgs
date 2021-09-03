027ba427c077846da7796e0e8df29d293f05ac4b
Thiago <thiago.carvalho@ufms.br>
2021-09-03 15:36:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,17 @@
+package com.example.orgs.dao
+
+import com.example.orgs.model.Produto
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto){
+        produtos.add(produto)
+    }
+    fun buscaTodos(): List<Produto>{
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+}
---
+++ app/src/main/java/com/example/orgs/ui/activity/ListaDeProdutosActivity.kt
@@ -0,0 +1,40 @@
+package com.example.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.ui.recyclerview.adapter.ListaProdutoAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaDeProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutoAdapter(context = this, produtos = dao.buscaTodos())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produto_fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produto_recyclerView)
+        recyclerView.adapter = adapter
+    }
+}
